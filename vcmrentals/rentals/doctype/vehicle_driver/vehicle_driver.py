538c9ebc3981b4e54603355672d5b78369e721de
pankajvcm244 <pankajvcm244@gmail.com>
2025-07-30 17:22:42 +0530

    Aman  Changes for komal meeting point

--- vcmrentals/rentals/doctype/vehicle_driver/vehicle_driver.py
+++ vcmrentals/rentals/doctype/vehicle_driver/vehicle_driver.py
@@ -3,7 +3,23 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, now_datetime
+from frappe.model.naming import make_autoname
 
 
 class VehicleDriver(Document):
-	pass
+	def autoname(self):
+			today = now_datetime()
+			date_prefix = today.strftime("%y%m")  # e.g., 2507 for July 2025
+			prefix = f"VD-"
+			self.name = make_autoname(prefix + ".#####")
+	
+
+
+
+
+
+
+
+
+
